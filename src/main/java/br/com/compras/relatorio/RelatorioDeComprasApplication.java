9678e94247cc22dc69dbe5ed04793a1ca44e765f
geovanefreitas <geovane.freitasc@gmail.com>
2024-11-15 12:04:00 -0300

    init commit

---
+++ src/main/java/br/com/compras/relatorio/RelatorioDeComprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.compras.relatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioDeComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compras/relatorio/RelatorioDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compras.relatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatorioDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
